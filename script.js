a19167d801781b5a90e43b84b6d48c30065a0d7b
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-03-27 18:16:22 +0500

    Add js

--- script.js
+++ script.js
@@ -8,12 +8,24 @@ function sendNumberValue(number) {
   calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
 }
 
+function addDecimal() {
+  if (!calculatorDisplay.textContent.includes('.')) {
+    calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+  }
+}
+
 inputBtns.forEach((inputBtn) => {
   if (inputBtn.cl.length === 0) {
     inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value))
   } else if (inputBtn.classList.contains('operator')) {
     inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value))
   } else if (inputBtn.classList.contains('decimal')) {
-    inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value))
+    inputBtn.addEventListener('click', () => addDecimal())
   } 
 })
+
+function resetAll() {
+  calculatorDisplay.textContent = '0';  
+}
+
+clearBtn.addEventListener('click', resetAll);
