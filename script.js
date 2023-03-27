deda05390287e976f95c453b4ccf9785bc4e5c6b
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-03-27 18:22:46 +0500

    Add js

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ const calculatorDisplay = document.querySelector('h1');
 const inputBtns = document.querySelectorAll('button');
 const clearBtn = document.getElementById('clear-btn');
 
-function sendNumberValue(number) {
-  const displayValue = calculatorDisplay.textContent;
+let firstValue = 0;
+let operatorValue = '';
+let awaitNextValue = false;
 
-  calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+function sendNumberValue(number) {
+  if (awaitNextValue) {
+    calculatorDisplay.textContent = number;
+    awaitNextValue = false;    
+  } else {
+    const displayValue = calculatorDisplay.textContent;
+
+    calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+  }
 }
 
 function addDecimal() {
@@ -14,17 +23,33 @@ function addDecimal() {
   }
 }
 
+function useOperator(operator) {
+  const currValue = Number(calculatorDisplay.textContent);
+
+  if (!firstValue) {
+    firstValue = currValue;
+  } else {
+
+  }
+
+  awaitNextValue = true;
+  operatorValue = operator;
+}
+
 inputBtns.forEach((inputBtn) => {
   if (inputBtn.cl.length === 0) {
     inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value))
   } else if (inputBtn.classList.contains('operator')) {
-    inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value))
+    inputBtn.addEventListener('click', () => useOperator(inputBtn.value))
   } else if (inputBtn.classList.contains('decimal')) {
     inputBtn.addEventListener('click', () => addDecimal())
   } 
 })
 
 function resetAll() {
+  firstValue = 0;
+  operatorValue = '';
+  awaitNextValue = false;
   calculatorDisplay.textContent = '0';  
 }
 
